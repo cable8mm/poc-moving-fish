7f3a7e50bd0eda51829fed260a5e24b77e23b6c2
Samgu Lee <cable8mm@gmail.com>
2024-02-23 17:58:55 +1300

    Formatting (#1)

--- Classes/BigFishSprite.h
+++ Classes/BigFishSprite.h
@@ -12,25 +12,18 @@
 #include "cocos2d.h"
 USING_NS_CC;
 
-enum eBigFishKey
-{
-    kBIG_FISH_YELLOW    = 0
-};
+enum eBigFishKey { kBIG_FISH_YELLOW = 0 };
 
-class BigFishSprite : public cocos2d::CCSprite
-{
+class BigFishSprite : public cocos2d::CCSprite {
 private:
-    enum eBigFishStats {
-        kBIG_FISH_READY,
-        kBIG_FISH_EATING,
-        kBIG_FISH_REMOVED
-    };
+    enum eBigFishStats { kBIG_FISH_READY, kBIG_FISH_EATING, kBIG_FISH_REMOVED };
     eBigFishKey _key;
-    eBigFishStats  _state;
+    eBigFishStats _state;
     int _ownPearlCount;  // 한번에 얻을 수 있는 진주 갯수
     void eatten();
     void onEnter();
     void close();
+
 public:
     static BigFishSprite* createWithKey(eBigFishKey k);
     float radiusForGoal;
--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -3,21 +3,22 @@
 
 #include "cocos2d.h"
 
-class HelloWorld : public cocos2d::CCLayer
-{
+class HelloWorld : public cocos2d::CCLayer {
 public:
-    // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
-    virtual bool init();  
+    // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of
+    // returning 'id' in cocos2d-iphone
+    virtual bool init();
 
-    // there's no 'id' in cpp, so we recommend returning the class instance pointer
-    static cocos2d::CCScene* scene();
-    
+    // there's no 'id' in cpp, so we recommend returning the class instance
+    // pointer
+    static cocos2d::CCScene *scene();
+
     // a selector callback
-    void menuCloseCallback(CCObject* pSender);
-    
+    void menuCloseCallback(CCObject *pSender);
+
     // implement the "static node()" method manually
     CREATE_FUNC(HelloWorld);
-    
+
 private:
     bool isLeft;
     bool isYegger;
@@ -26,13 +27,14 @@ private:
     unsigned int deltaSum;
     cocos2d::CCArray *displayPearls;
     cocos2d::CCPoint prevBigFishPoint;
-    
+
     bool ccTouchBegan(cocos2d::CCTouch *touch, cocos2d::CCEvent *pEvent);
     void ccTouchMoved(cocos2d::CCTouch *touch, cocos2d::CCEvent *event);
     void ccTouchEnded(cocos2d::CCTouch *touch, cocos2d::CCEvent *event);
     void ccTouchCancelled(cocos2d::CCTouch *touch, cocos2d::CCEvent *pEvent);
     void registerWithTouchDispatcher();
-    void goPoint(cocos2d::CCPoint pt, bool isYegger = false, float termFishCrossSecond = 1.);
+    void goPoint(cocos2d::CCPoint pt, bool isYegger = false,
+                 float termFishCrossSecond = 1.);
     void goPoint2(cocos2d::CCPoint pt);
     void callFish();
     void endYegger();
@@ -40,4 +42,4 @@ private:
     void resetHeartbeat();
 };
 
-#endif // __HELLOWORLD_SCENE_H__
+#endif  // __HELLOWORLD_SCENE_H__
